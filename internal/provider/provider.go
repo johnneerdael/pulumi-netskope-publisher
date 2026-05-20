7bbe7026d54b358c5939caacd01e54409e1d69b3
John Neerdael <jneerdael@netskope.com>
2026-05-21 00:19:38 +0200

    chore(release): v0.2.0

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -79,7 +79,7 @@ func Schema(ctx context.Context, version int) (string, error) {
 	server, err := integration.NewServer(
 		ctx,
 		Name,
-		semver.MustParse("0.1.12"),
+		semver.MustParse("0.2.0"),
 		integration.WithProvider(provider),
 	)
 	if err != nil {
